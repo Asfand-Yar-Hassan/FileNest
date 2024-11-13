7ea509a3bed667270494697e9d3217ab2d072a1f
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-11-12 19:06:26 -0500

    Refactor

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,7 +34,7 @@ MINIO_SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
