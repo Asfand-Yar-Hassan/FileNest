86d3ed11c6f39995d7d86d1420a1ee12b9f265cc
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2025-03-13 17:49:03 -0400

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,10 +6,11 @@ const nextConfig = {
     domains: ['localhost', '127.0.0.1', '54.162.76.167'],
   },
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://54.162.76.167:8000';
     return [
       {
         source: '/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+        destination: `${apiUrl}/:path*`,
       },
     ];
   },
